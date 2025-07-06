7585399d364e01f1b29157529fb1dec7601a6e13
Thunggg <thuannguyen20041028@gmail.com>
2025-07-06 07:54:13 +0700

    Sử dụng State và Data Type

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState<string>("hoidanit");
+  const [age, setAge] = useState<number>(30);
+  const test = false;
+  const [person, setPerson] = useState<{
+    name: string,
+    age: number
+  }>({
+    name: "hoidanit",
+    age: 25
+  })
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.text}>Test</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
       </View>
       <Text>Hello world</Text>
     </View>
@@ -20,7 +34,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20
   },
 });
